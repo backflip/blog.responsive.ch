f36006c30c45902abb9c202a164aae2e0c8d3a6b
Thomas Jaggi <thomas@responsive.ch>
2022-11-18 20:19:12 +0100

    feat: add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blog.responsive.ch",
   integrations: [
     mdx(),
     image({
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,23 @@
+import rss from "@astrojs/rss";
+
+const postImportResult = import.meta.glob("../pages/**/*.mdx", { eager: true });
+const posts = Object.values(postImportResult);
+
+export const get = () =>
+  rss({
+    title: "blog.responsive.ch",
+    description: "A blog about web technology and remotely related thingies.",
+    site: import.meta.env.SITE,
+    items: posts
+      .filter((post) => !post.frontmatter.draft)
+      .sort(
+        (a, b) =>
+          Date.parse(b.frontmatter.date) - Date.parse(a.frontmatter.date)
+      )
+      .map((post) => ({
+        link: post.url,
+        title: post.frontmatter.title,
+        pubDate: post.frontmatter.date,
+      })),
+    customData: `<language>en-us</language>`,
+  });
